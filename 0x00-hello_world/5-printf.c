c5515e7c494b486eb7b7b0d6bdcf1d7037361799
keroojoseph <keroojoseph0@gmail.com>
2023-09-08 20:46:59 +0300

    ADD file 5-print.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main - entery point
+ *
+ *Return: 0 is seccess
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
