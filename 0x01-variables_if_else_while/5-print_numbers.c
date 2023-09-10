06a0ac9f69fc1361e0f62825bffa670d66931e37
keroojoseph <keroojoseph0@gmail.com>
2023-09-10 18:46:57 +0300

    ADD file 5-print_number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - Entery point
+ *Description: print digit base 10
+ *Return: 0 is seccess
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
