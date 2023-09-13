29b74483ab520b909e6fd8e6b542f4881849b0fb
keroojoseph <keroojoseph0@gmail.com>
2023-09-13 23:01:23 +0300

    UPDATE file 9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,22 +11,21 @@ void times_table(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		_putchar(48);
+		for (j = 1; j <= 9; j++)
 		{
+			_putchar(',');
+			_putchar(' ');
 			sum = i * j;
 			if (sum > 9)
 			{
 				_putchar(sum / 10 + 48);
 				_putchar(sum % 10 + 48);
-				_putchar(',');
-				_putchar(' ');
 			}
 			else
 			{
+				_putchar(' ');
 				_putchar(sum + 48);
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
 			}
 		}
 		_putchar('\n');
