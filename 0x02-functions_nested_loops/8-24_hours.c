6b9cc278beb356a7ec71555476e2ac8ac862c6e9
keroojoseph <keroojoseph0@gmail.com>
2023-09-13 21:52:39 +0300

    ADD 8-24time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *jack_bauer - function time
+ *Description: function compute time
+*/
+
+void jack_bauer(void)
+{
+	char h, m;
+
+	for (h = 0; h <= 23; h++)
+	{
+		for (m = 0; m <= 59; m++)
+		{
+			_putchar((h / 10) + 48);
+			_putchar((h % 10) + 48);
+			_putchar(':');
+			_putchar((m / 10) + 48);
+			_putchar((h % 10) + 48);
+			_putchar('\n');
+		}
+	}
+}
