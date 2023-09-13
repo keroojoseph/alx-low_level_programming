4d25ed100a281ab4095cd3c51274603e4bb8d374
keroojoseph <keroojoseph0@gmail.com>
2023-09-13 21:57:27 +0300

    UPDATE file 8-24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -17,7 +17,7 @@ void jack_bauer(void)
 			_putchar((h % 10) + 48);
 			_putchar(':');
 			_putchar((m / 10) + 48);
-			_putchar((h % 10) + 48);
+			_putchar((m % 10) + 48);
 			_putchar('\n');
 		}
 	}
