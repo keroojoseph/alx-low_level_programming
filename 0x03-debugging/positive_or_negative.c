65ce395f06d49d09c24f3560084deddfe0e03091
keroojoseph <keroojoseph0@gmail.com>
2023-09-14 12:54:08 +0300

    ADD FILE 0

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,26 +1,23 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include "main.h"
 
 /**
- *main - Entry point
+ *positive_or_negative - Entry point
  *
  *Description: print random number
- *Return: 0 is seccess
 */
 
 void positive_or_negative(int i)
 {
-        if (i > 0)
-        {
-                printf("%d is positive\n", i);
-        }
-        else if (i == 0)
-        {
-                printf("%d is zero\n", i);
-        }
-        else if (i < 0)
-        {
-                printf("%d is negative\n", i);
-        }
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+	else if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
 }
