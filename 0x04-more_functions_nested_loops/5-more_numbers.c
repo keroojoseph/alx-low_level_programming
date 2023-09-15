0b95d2e758431d0ac2b778509605f4abef429f9b
keroojoseph <keroojoseph0@gmail.com>
2023-09-15 13:33:36 +0300

    ADD function 5-more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *more_numbers - function print intgere for 0 to 14
+*/
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if( j >= 10)
+			{
+				_putchar(j / 10 + 48);
+				_putchar(j % 10 + 48);
+			}
+			else
+			{
+				_putchar('0' + j);
+			}
+		}
+		_putchar('\n');
+	}
+}
