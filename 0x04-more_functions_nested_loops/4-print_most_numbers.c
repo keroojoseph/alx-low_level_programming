c699ad5f9eae56da9bb16f96af979563bb7a7559
keroojoseph <keroojoseph0@gmail.com>
2023-09-15 12:44:59 +0300

    ADD function 4-print_most_numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *print_most_numbers - function print most number without 2 && 4
+*/
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + 48);
+		}
+	}
+	_putchar('\n');
+}
