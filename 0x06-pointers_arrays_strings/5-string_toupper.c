58f98e77cd8c7340e8a54913ee78434bee75f1dd
keroojoseph <keroojoseph0@gmail.com>
2023-09-21 17:27:43 +0300

    add file 5-string_toupper and 5-main.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *string_toupper - This function will replace all lowercase letters in
+ *the string pointed by str to uppercase
+ *@str: The string that will be checked for lowercase letters
+ *
+ *Return: The resulting string str, where all the letters are uppercase
+*/
+
+char *string_toupper(char *str)
+{
+    int i;
+    for (i = 0; str[i] != '\0'; i++)
+    {
+            if (str[i] >= 'a' && str[i] <= 'z')
+                    str[i] =(int)str[i] - 32;
+    }
+    return (str);
+ }
