cbc64035359fe161120e4588d53c35078f3f8ed0
keroojoseph <keroojoseph0@gmail.com>
2023-09-21 17:35:14 +0300

    update betty 5-sreing_toupper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,11 +10,12 @@
 
 char *string_toupper(char *str)
 {
-    int i;
-    for (i = 0; str[i] != '\0'; i++)
-    {
-            if (str[i] >= 'a' && str[i] <= 'z')
-                    str[i] =(int)str[i] - 32;
-    }
-    return (str);
- }
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = (int)str[i] - 32;
+	}
+	return (str);
+}
