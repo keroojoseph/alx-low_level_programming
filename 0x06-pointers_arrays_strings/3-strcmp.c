9d4c83bded0e03e17df591d553178a5439f1e915
keroojoseph <keroojoseph0@gmail.com>
2023-09-21 15:51:05 +0300

    update file 3-strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,12 +9,14 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	if (strcmp(s1, s2) == 0)
+	while (*s1 != '\0' && *s2 != '\0')
 	{
-		return (0);
+		if (*s1 != *s2)
+		{
+			return (*s1 - *s2);
+		}
+		s1++;
+		s2++;
 	}
-	else if (strcmp(s1, s2) < 0)
-		return (-15);
-	else
-		return (15);
+	return (0);
 }
