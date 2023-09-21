b6270b7d034dbab4bbdae390c34736acb929499d
keroojoseph <keroojoseph0@gmail.com>
2023-09-21 12:03:30 +0300

    add file 3-puts and 3-main.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ *_puts - function print use puts
+ *@str: input parameter
+*/
+
+void _puts(char *str)
+{
+	puts(str);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,9 +2,11 @@
 #define MAIN_H
 
 #include <string.h>
+#include <stdio.h>
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void _puts(char *str);
 
 
 
