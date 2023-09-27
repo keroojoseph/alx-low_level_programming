c612e2f5651ee9ecef28138b5e8ce1e9ffd7b02f
keroojoseph <keroojoseph0@gmail.com>
2023-09-27 20:24:17 +0300

    add file 2-strchar search for character

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,12 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <string.h>
-#include <stdio.h>
 
-int _putchar(char c);
-char *_memset(char *s, char b, unsigned int n);
+#include <stdio.h>
+#include <string.h>
+
+char *_strchr(char *s, char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memset(char *s, char b, unsigned int n);
+
+
+
+
 
 
 
