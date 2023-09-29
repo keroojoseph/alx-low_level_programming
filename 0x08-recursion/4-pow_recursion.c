72c63c74f82922d4c0e62f0e85068535e7717365
keroojoseph <keroojoseph0@gmail.com>
2023-09-29 15:21:41 +0300

    update betty and code file 4-pow_recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,11 +1,21 @@
 #include "main.h"
 
+/**
+ *_pow_recursion - function that returns the value of x raised to the pow of y
+ *@x: input one the base
+ *@y: input two the power
+ *Return: x ** y
+*/
 
 int _pow_recursion(int x, int y)
 {
-    if (y < 0)
-    {
-        return (-1);
-    }
-    return (x * _pow_recursion(x, y - 1));
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
 }
