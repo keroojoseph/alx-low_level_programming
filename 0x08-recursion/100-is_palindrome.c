35279e3b8dc01ad23301ceb25cca433760a4ffe0
keroojoseph <keroojoseph0@gmail.com>
2023-10-01 19:00:18 +0300

    add file 100-is_palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,1 +1,25 @@
+#include "main.h"
 
+
+
+int help(char *s, int start, int end)
+{
+	if (start == end)
+		return (1);
+	if (start > end)
+		return (1);
+	if (s[start] != s[end])
+		return (0);
+	return (help(s, start + 1, end -1));
+}
+
+
+int is_palindrome(char *s)
+{
+	int n = strlen(s);
+
+	if (n == 0)
+		return (1);
+	else
+		return (help(s, 0, n - 1));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <string.h>
+#include <stdio.h>
+
 int factorial(int n);
 int _putchar(char c);
 void _puts_recursion(char *s);
@@ -11,7 +14,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
-
+int is_palindrome(char *s);
 
 
 #endif
