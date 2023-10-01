daac653340dc54c9b7ce1b6e3bae3d15f06799ef
keroojoseph <keroojoseph0@gmail.com>
2023-10-01 17:25:18 +0300

    add file 5-sqrt_recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,26 +1,28 @@
 #include "main.h"
 
 /**
- *_sqrt_recursion - function return nuture root
- *@n: input number
- *Return: the result of root
+ *help - function help checker sqrt
+ *@n: base case
+ *@base: base checker
+ *Return: natural square root
 */
 
-int _sqrt_recursion(int n) {
-  if (n < 0) {
-    return -1;
-  } else if (n == 0 || n == 1) {
-    return n;
-  } else {
-    
-    int approx = _sqrt_recursion(n / 2);
+int help(int n, int base)
+{
+	if (n * n == base)
+		return (n);
+	if (n * n > base)
+		return (-1);
+	return (help(n + 1, base));
+}
 
-   
-    if (approx * approx == n) {
-      return approx;
-    } else {
-     
-      return _sqrt_recursion((approx + n / approx) / 2);
-    }
-  }
+/**
+ *_sqrt_recursion - function return sqare root of n
+ *@n: input number
+ *Return: square root
+*/
+
+int _sqrt_recursion(int n)
+{
+	return (help(1, n));
 }
