57fd79f4704013232113dcfc14dc7361db32bf43
keroojoseph <keroojoseph0@gmail.com>
2023-10-06 16:54:40 +0300

    update betty

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - entery function
+ * @argc: number of argument command line
+ * @argv: vector
+ * Return: 0 is success
+*/
+
 int main(int argc, char *argv[])
 {
 	(void)argc;
