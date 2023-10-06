a0776ab90c8e163236055fd8e7bc0301426293e7
keroojoseph <keroojoseph0@gmail.com>
2023-10-06 17:45:08 +0300

    add file 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: number of argument
+ * @argv: array of string
+ * Return: 0 is seccess
+*/
+
+int main(int argc, char *argv[])
+{
+    int i;
+
+    printf("%d\n", argc - 1);
+    for (i = 1; i < argc - 1; i++)
+    {
+        printf("%s", argv[i]);
+    }
+    return (0);
+}
