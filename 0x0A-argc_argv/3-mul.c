b4f587ad18b4264a45f7f8285d51e93b67483f6f
keroojoseph <keroojoseph0@gmail.com>
2023-10-07 15:30:09 +0300

    ubdate file 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -15,12 +15,15 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	int a = atoi(argv[1]);
-	int b = atoi(argv[2]);
+	int a, b;
 
-	int result = a * b;
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
 
-	printf("%d\n", result);
-
+	if (argc == 3)
+	{
+		int result = a * b;
+		printf("%d\n", result);
+	}
 	return (0);
 }
