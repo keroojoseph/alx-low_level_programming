c8697a0f29f5ced5b4c01d2b0b3649e9ebcfb009
keroojoseph <keroojoseph0@gmail.com>
2023-10-12 00:19:45 +0300

    add file 2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ *print_dog - function
+ *@d: first member
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		printf("Name: %s\n", d->name ? d->name : "(nil)");
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+	}
+}
