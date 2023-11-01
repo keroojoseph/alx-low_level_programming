a84d6e1692dcaf416e55598fa0827d5e777ab7dc
keroo joseph <keroojoseph0@gmail.com>
2023-11-01 17:03:19 +0200

    add file 2-str_concat

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * str_concat - function concatenation string
+ * @s1: argument one
+ * @s2: argument two
+ * Return: str
+**/
+
+char *str_concat(char *s1, char *s2)
+{
+	int len_s1 = strlen(s1);
+	int len_s2 = strlen(s2);
+	int m = len_s1 + len_s2 + 1;
+	char *str = malloc(m);
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	if (str != NULL)
+	{
+		strcpy(str, s1);
+		strcat(str, s2);
+	}
+	else
+		return (NULL);
+	return (str);
+}
